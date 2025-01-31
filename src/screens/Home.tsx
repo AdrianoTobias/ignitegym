19a1e12bf4016464daa9275320ae40b8d0f6f021
Adriano <adrianoact@msn.com>
2025-01-30 22:42:18 -0300

    Interfaces: selected group style

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,28 @@
-import { VStack } from '@gluestack-ui/themed'
+import { HStack, VStack } from '@gluestack-ui/themed'
 import { HomeHeader } from '@components/HomeHeader'
 import { Group } from '@components/Group'
+import { useState } from 'react'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name="Costas" isActive={false} />
+      <HStack>
+        <Group
+          name="Costas"
+          isActive={groupSelected === 'costas'}
+          onPress={() => setGroupSelected('costas')}
+        />
+
+        <Group
+          name="Ombro"
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
     </VStack>
   )
 }
