792638ec6c38ec8ae63caea42101f6cb4c2de013
Adriano <adrianoact@msn.com>
2025-02-01 16:48:56 -0300

    Forms: inputs with states

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { Box } from '@gluestack-ui/themed'
 
-import { AppRoutes } from './app.routes'
+import { AuthRoutes } from './auth.routes'
 
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 
@@ -12,7 +12,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AppRoutes />
+        <AuthRoutes />
       </NavigationContainer>
     </Box>
   )
