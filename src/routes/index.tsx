be69240971039cf312185b5e784999ab8732ba11
Adriano <adrianoact@msn.com>
2025-02-02 18:49:48 -0300

    React Native Contexts: redirect loged user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,12 @@ import { Box } from '@gluestack-ui/themed'
 import { useAuth } from '@hooks/useAuth'
 
 import { AuthRoutes } from './auth.routes'
+import { AppRoutes } from './app.routes'
 
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 
 export function Routes() {
   const { user } = useAuth()
-
-  console.log('USUÁRIO LOGADO =>', user)
 
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
@@ -18,7 +17,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
