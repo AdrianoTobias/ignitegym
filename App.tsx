df33d1026701fd6d13f198235bd40dab2a7b2e1e
Adriano <adrianoact@msn.com>
2025-02-02 17:33:11 -0300

    React Native Contexts: create context

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@components/Loading'
 import { Routes } from '@routes/index'
+import { AuthContext } from '@contexts/AuthContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -21,7 +22,16 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <AuthContext.Provider
+        value={{
+          id: '1',
+          name: 'Teste 1',
+          email: 'teste1@teste.com.br',
+          avatar: 'teste.png',
+        }}
+      >
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContext.Provider>
     </GluestackUIProvider>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,19 @@
+import { useContext } from 'react'
+
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { Box } from '@gluestack-ui/themed'
+
+import { AuthContext } from '@contexts/AuthContext'
 
 import { AuthRoutes } from './auth.routes'
 
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 
 export function Routes() {
+  const contextData = useContext(AuthContext)
+
+  console.log('USUÁRIO LOGADO =>', contextData)
+
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
 
