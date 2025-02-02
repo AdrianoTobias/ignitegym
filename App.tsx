e481e7a640aac71738dfa59a93baf34a684e552b
Adriano <adrianoact@msn.com>
2025-02-02 17:46:06 -0300

    React Native Contexts: unifying create and provider context

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@components/Loading'
 import { Routes } from '@routes/index'
-import { AuthContext } from '@contexts/AuthContext'
+import { AuthContextProvider } from '@contexts/AuthContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -22,18 +22,9 @@ export default function App() {
         translucent
       />
 
-      <AuthContext.Provider
-        value={{
-          user: {
-            id: '1',
-            name: 'Teste 1',
-            email: 'teste1@teste.com.br',
-            avatar: 'teste.png',
-          },
-        }}
-      >
+      <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </GluestackUIProvider>
   )
 }
