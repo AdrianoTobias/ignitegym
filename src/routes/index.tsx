c022aa1a4d297ab8639c7c27deb9f9f39872e44b
Adriano <adrianoact@msn.com>
2025-02-02 17:51:53 -0300

    React Native Contexts: create useAuth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,16 @@
-import { useContext } from 'react'
-
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { Box } from '@gluestack-ui/themed'
 
-import { AuthContext } from '@contexts/AuthContext'
+import { useAuth } from '@hooks/useAuth'
 
 import { AuthRoutes } from './auth.routes'
 
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 
 export function Routes() {
-  const contextData = useContext(AuthContext)
+  const { user } = useAuth()
 
-  console.log('USUÁRIO LOGADO =>', contextData)
+  console.log('USUÁRIO LOGADO =>', user)
 
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
