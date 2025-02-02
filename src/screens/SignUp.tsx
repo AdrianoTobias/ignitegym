70af174be5f622de0afc41f8b7323548778542af
Adriano <adrianoact@msn.com>
2025-02-02 16:53:40 -0300

    HTTP Client: Axios Exceptions

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { Alert } from 'react-native'
 import {
   Center,
   Heading,
@@ -11,6 +12,7 @@ import * as yup from 'yup'
 
 import { useNavigation } from '@react-navigation/native'
 
+import axios from 'axios'
 import { api } from '@services/api'
 
 import BackgroundImg from '@assets/background.png'
@@ -56,8 +58,14 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await api.post('/users', { name, email, password })
-    console.log(response.data)
+    try {
+      const response = await api.post('/users', { name, email, password })
+      console.log(response.data)
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message)
+      }
+    }
   }
 
   return (
