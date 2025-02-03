ea58182ec60795da58af4201a045f202715e1dc4
Adriano <adrianoact@msn.com>
2025-02-02 22:09:44 -0300

    Consuming the back-end: getting and listing groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,17 @@
-import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { Heading, HStack, Text, useToast, VStack } from '@gluestack-ui/themed'
 import { HomeHeader } from '@components/HomeHeader'
 import { Group } from '@components/Group'
+import { ToastMessage } from '@components/ToastMessage'
 import { ExerciseCard } from './ExerciseCard'
 
 import { useNavigation } from '@react-navigation/native'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FlatList } from 'react-native'
+
+import { api } from '@services/api'
+import { AppError } from '@utils/AppError'
 
 export function Home() {
   const [exercises, setExercises] = useState([
@@ -19,14 +23,44 @@ export function Home() {
     'Rosca',
     'Levantamento terra',
   ])
-  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [groups, setGroups] = useState<string[]>([])
   const [groupSelected, setGroupSelected] = useState('costas')
+
+  const toast = useToast()
 
   const navigation = useNavigation<AppNavigatorRoutesProps>()
 
   function handleOpenExerciseDetails() {
     navigation.navigate('exercise')
   }
+
+  async function fetchGroups() {
+    try {
+      const response = await api.get('/groups')
+      setGroups(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar os grupos musculares'
+
+      toast.show({
+        placement: 'top',
+        render: ({ id }) => (
+          <ToastMessage
+            id={id}
+            action="error"
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      })
+    }
+  }
+
+  useEffect(() => {
+    fetchGroups()
+  }, [])
 
   return (
     <VStack flex={1}>
