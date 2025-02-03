e7c15a8d8d4286e0d8d2b15fee09ee0fa2189973
Adriano <adrianoact@msn.com>
2025-02-02 22:25:58 -0300

    Consuming the back-end: ExerciseDTO

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -12,9 +12,10 @@ import { FlatList } from 'react-native'
 
 import { api } from '@services/api'
 import { AppError } from '@utils/AppError'
+import { ExerciseDTO } from '@dtos/ExerciseDTO'
 
 export function Home() {
-  const [exercises, setExercises] = useState([])
+  const [exercises, setExercises] = useState<ExerciseDTO[]>([])
   const [groups, setGroups] = useState<string[]>([])
   const [groupSelected, setGroupSelected] = useState('costas')
 
@@ -53,7 +54,7 @@ export function Home() {
   async function fecthExercisesByGroup() {
     try {
       const response = await api.get(`/exercises/bygroup/${groupSelected}`)
-      console.log(response.data)
+      setExercises(response.data)
     } catch (error) {
       const isAppError = error instanceof AppError
       const title = isAppError
@@ -116,7 +117,7 @@ export function Home() {
 
         <FlatList
           data={exercises}
-          keyExtractor={(item) => item}
+          keyExtractor={(item) => item.id}
           renderItem={() => (
             <ExerciseCard onPress={handleOpenExerciseDetails} />
           )}
